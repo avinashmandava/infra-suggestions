05b5f154cfa4c90f24dacfe5bdfa3b9fde017e2f
Avinash Mandava <avinash.mandava@gmail.com>
2023-03-31 16:09:26 -0700

    working end to end

---
+++ data_processing/format_metrics.py
@@ -0,0 +1,25 @@
+from external_apis import datadog
+import numpy as np
+
+def get_formatted_metrics(technology):
+  if technology == 'PostgreSQL':
+    metrics_list = ['postgresql.rows_returned', 'postgresql.rows_updated']
+  else:
+    raise ValueError(f'No metrics found for {technology}')
+  metric_data = {}
+  for metric in metrics_list:
+    data = datadog.get_time_series('avg', metric)['data']['attributes']['values'][0]
+    current = data[-1]
+    mean = np.mean(data)
+    std = np.std(data)
+    z = (current-mean)/std
+    metric_data[metric] = {
+      'mean': mean,
+      'std': std,
+      'current_value': current,
+      'z_score': z
+    }
+
+  return metric_data
+
+
--- scratch.py
+++ scratch.py
@@ -1,22 +1,25 @@
 import openai
 import langchain
 import os
+import json
 from dotenv import load_dotenv
 
-from data_processing import prompt_generator
+from data_processing import format_metrics
 from external_apis import datadog
+from llm_integration import prompt_generator
 
-x = prompt_generator.create_prompt()
 
-print(x)
-'''
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_KEY')
-print(OPENAI_API_KEY)
-# Basic use case - calling LLM on some input
 
+# Config
+technology = "PostgreSQL"
+optimization_goal = "improve read performance"
+metric_data = json.dumps(format_metrics.get_formatted_metrics(technology))
+
+# Basic use case - calling LLM on some input
 from langchain.llms import OpenAI
-llm = OpenAI(model_name="text-ada-001", temperature=0.9)
-text = "How can I improve the performance of my Cassandra cluster?"
-#print(llm(text))
-'''
+llm = OpenAI(model_name="text-davinci-003", temperature=1)
+prompt = prompt_generator.construct_prompt(technology, optimization_goal, metric_data)
+response = llm(prompt)
+print(response)
